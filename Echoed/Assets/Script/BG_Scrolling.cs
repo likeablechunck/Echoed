f7707f79aff5874c03c1ecb15925e144fc3c195d
faranak <faranak.rzv@gmail.com>
2016-09-13 17:41:24 -0700

    Added scrolling BG and follow path

---
+++ Echoed/Assets/Script/BG_Scrolling.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BG_Scrolling : MonoBehaviour {
+
+    public float speed;
+    public float x_boarder;
+    public float x_initial_position;
+
+    // Use this for initialization
+    void Start()
+    {
+        x_boarder = -13f;
+        x_initial_position = -2f;
+        this.transform.position = new Vector3(x_initial_position, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        if (player != null)
+        {
+            this.transform.Translate(speed * -Time.deltaTime, 0, 0);
+            print("Background is at: " + transform.position);
+            // If past a certain point on X
+            if (this.transform.position.x <= x_boarder)
+            {
+                print("Background passed the boundries");
+                // Jump far to the right
+                //distance between the boundry and the initial point(in X)
+                this.transform.Translate(-(this.transform.position.x - x_initial_position), 0, 0);
+            }
+        }
+    }
+
+}
---
+++ Echoed/Assets/Script/Player_Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        //iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(""), "time", 5));
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Up"), "time", 3, "easetype", iTween.EaseType.easeInOutSine));
+            //iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Up"), "time", 3));
+
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Down"), "time", 3, "easetype", iTween.EaseType.easeInOutSine));
+        }
+	
+
+	}
+}
