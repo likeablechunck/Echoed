fe6e68a759b714331cf0bb49019aa7f6b0b26c84
faranak <faranak.rzv@gmail.com>
2016-09-18 13:39:45 -0700

    Added Tutorial_Control and Tutorial_Signal scripts!
    Not Done

--- Echoed/Assets/Script/Tutorial_Control.cs
+++ Echoed/Assets/Script/Tutorial_Control.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tutorial_Control : MonoBehaviour {
+public class Tutorial_Control : MonoBehaviour
+{
+    public ArrayList whatToInstantiate = new ArrayList();
+    public int n = 1;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        for (int i = 0; i < 10 * n ; i=i+10)
+        {
+            whatToInstantiate.Add("Empty_Circle");
+            whatToInstantiate.Add("Empty_Circle");
+            whatToInstantiate.Add("Filled_Circle");
+            whatToInstantiate.Add("Filled_Circle");
+            whatToInstantiate.Add("Up_Arrow");
+            whatToInstantiate.Add("Empty_Circle");
+            whatToInstantiate.Add("Empty_Circle");
+            whatToInstantiate.Add("Filled_Circle");
+            whatToInstantiate.Add("Filled_Circle");
+            whatToInstantiate.Add("Down_Arrow");
+
+        }
+
+    }
 }
