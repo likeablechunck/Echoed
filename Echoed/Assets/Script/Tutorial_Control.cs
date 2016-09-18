bf041c22b8d78d324caca111aadca501a2dd174c
faranak <faranak.rzv@gmail.com>
2016-09-18 16:46:20 -0700

    Wroking on signals instantiation

--- Echoed/Assets/Script/Tutorial_Control.cs
+++ Echoed/Assets/Script/Tutorial_Control.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Tutorial_Control : MonoBehaviour
 {
     public ArrayList whatToInstantiate = new ArrayList();
-    public int n = 1;
+    public int n = 3;
+    public float i;
 
 	// Use this for initialization
 	void Start ()
@@ -15,7 +16,11 @@ public class Tutorial_Control : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        for (int i = 0; i < 10 * n ; i=i+10)
+        i += Time.deltaTime;
+        //this will be the order of signals that will appear on screen
+        //empty empty fill fill arrow
+        //This will repeat for 30secs
+        for (int i = 0; i < 10 * n; i = i + 10)
         {
             whatToInstantiate.Add("Empty_Circle");
             whatToInstantiate.Add("Empty_Circle");
@@ -30,5 +35,9 @@ public class Tutorial_Control : MonoBehaviour
 
         }
 
+        Instantiate(GameObject.Find((string)whatToInstantiate[(int)i]),new Vector2(11,-4.2f), Quaternion.identity);
+        
     }
+
+    
 }
