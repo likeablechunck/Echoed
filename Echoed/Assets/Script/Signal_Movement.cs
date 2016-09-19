dd49d983562c0b7129c47e7adbfbb8ee4d491ace
faranak <faranak.rzv@gmail.com>
2016-09-18 19:28:36 -0700

    work in progress to instantiate the signals

---
+++ Echoed/Assets/Script/Signal_Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Signal_Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.position = new Vector2(11,-4.2f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	
+	}
+}
--- Echoed/Assets/Script/Tutorial_Control.cs
+++ Echoed/Assets/Script/Tutorial_Control.cs
@@ -4,40 +4,62 @@ using System.Collections;
 public class Tutorial_Control : MonoBehaviour
 {
     public ArrayList whatToInstantiate = new ArrayList();
-    public int n = 3;
-    public float i;
+    public int numberOfLoops;
+    int timeElapsedInSecs;
+    GameObject tempObj;
+    public float timeElapsed;
 
 	// Use this for initialization
 	void Start ()
     {
+        numberOfLoops = 3;
+        timeElapsedInSecs = 0;
+        timeElapsed = 0;
+        for (int i = 0; i < 10 * numberOfLoops; i = i + 10)
+        {
+            whatToInstantiate.Add("Resources/Prefab/Empty_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Empty_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Filled_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Filled_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Up_Arrow");
+            whatToInstantiate.Add("Resources/Prefab/Empty_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Empty_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Filled_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Filled_Circle");
+            whatToInstantiate.Add("Resources/Prefab/Down_Arrow");
+        }
+        print("array has " + whatToInstantiate.Count + "elements");
+        print("elements : " + whatToInstantiate);
 
     }
+
 	
 	// Update is called once per frame
 	void Update ()
     {
-        i += Time.deltaTime;
+        
+        timeElapsed = timeElapsed + Time.deltaTime;
         //this will be the order of signals that will appear on screen
         //empty empty fill fill arrow
         //This will repeat for 30secs
-        for (int i = 0; i < 10 * n; i = i + 10)
+
+        if ((timeElapsed - timeElapsedInSecs) > 1.0f)
         {
-            whatToInstantiate.Add("Empty_Circle");
-            whatToInstantiate.Add("Empty_Circle");
-            whatToInstantiate.Add("Filled_Circle");
-            whatToInstantiate.Add("Filled_Circle");
-            whatToInstantiate.Add("Up_Arrow");
-            whatToInstantiate.Add("Empty_Circle");
-            whatToInstantiate.Add("Empty_Circle");
-            whatToInstantiate.Add("Filled_Circle");
-            whatToInstantiate.Add("Filled_Circle");
-            whatToInstantiate.Add("Down_Arrow");
-
+            print("Time elapsed is: " + timeElapsed);
+            string objectToInstantiateName = (string)whatToInstantiate[timeElapsedInSecs];
+            GameObject objectToInstantiate = GameObject.Find(objectToInstantiateName);
+            if (objectToInstantiate != null)
+            {
+                tempObj = (Instantiate(objectToInstantiate,
+                    new Vector2(11, -4.2f),
+                    Quaternion.identity)) as GameObject;
+            } else
+            {
+                print("you wanted me to instantiate an object named " + objectToInstantiateName + " but i did not find it");
+            }
+            timeElapsedInSecs++;
+            print("signal that was just instantiated was :" + tempObj);
+            print("Time elapsed is: " + timeElapsedInSecs);
         }
-
-        Instantiate(GameObject.Find((string)whatToInstantiate[(int)i]),new Vector2(11,-4.2f), Quaternion.identity);
-        
     }
-
-    
 }
